c2aa30b5c5aaa59aee11199e97f7d26e707d26f4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-02-21 11:11:31 +0100

    Differentiate between various states

--- src/WheelOfSuggestions.elm
+++ src/WheelOfSuggestions.elm
@@ -14,16 +14,25 @@ main =
         }
 
 
-type alias Model =
-    { message : String
-    }
+type Model
+    = Initializing
+    | Ready Topics
+    | Presenting Suggestion Topics
+
+
+type alias Topics =
+    { suggestions : List Suggestion }
+
+
+type alias Suggestion =
+    { topic : String }
 
 
 init : flag -> ( Model, Cmd Msg )
 init _ =
     let
         model =
-            { message = "Hello, World!" }
+            Ready { suggestions = [ { topic = "TDD like you meant it" } ] }
     in
     ( model, Cmd.none )
 
@@ -40,8 +49,39 @@ update msg model =
 
 
 view : Model -> Html Msg
-view { message } =
-    Html.text message
+view model =
+    case model of
+        Initializing ->
+            viewInitializing
+
+        Ready _ ->
+            viewReady
+
+        Presenting suggestion _ ->
+            viewSuggestion suggestion
+
+
+viewInitializing : Html msg
+viewInitializing =
+    Html.text "Fetching topics"
+
+
+viewReady : Html Msg
+viewReady =
+    Html.div []
+        [ Html.p []
+            [ Html.text "Are you feeling lucky?"
+            ]
+        ]
+
+
+viewSuggestion : Suggestion -> Html Msg
+viewSuggestion { topic } =
+    Html.div []
+        [ Html.p []
+            [ Html.text topic
+            ]
+        ]
 
 
 subscriptions : Model -> Sub Msg
