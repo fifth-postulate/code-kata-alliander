660d7f39318539fd1418dc8335e3f5d7603d23e8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2024-02-21 11:02:46 +0100

    Create an actual elm element

--- src/WheelOfSuggestions.elm
+++ src/WheelOfSuggestions.elm
@@ -1,8 +1,49 @@
 module WheelOfSuggestions exposing (..)
 
+import Browser
 import Html exposing (Html)
 
 
-main : Html msg
+main : Program () Model Msg
 main =
-    Html.text "Hello, World!"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { message : String
+    }
+
+
+init : flag -> ( Model, Cmd Msg )
+init _ =
+    let
+        model =
+            { message = "Hello, World!" }
+    in
+    ( model, Cmd.none )
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        None ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view { message } =
+    Html.text message
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
